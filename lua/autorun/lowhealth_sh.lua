9e1de2e3ec99b2168fa95dffb667baa03d0b000e
Anaël Chevillard <anaelchevillard@gmail.com>
2024-04-20 12:24:57 +0200

    Update lowhealth_sh.lua by dvpweb

--- lua/autorun/lowhealth_sh.lua
+++ lua/autorun/lowhealth_sh.lua
@@ -25,11 +25,11 @@ CreateConVar( "etb_redflash", 1, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY}, "0 = 
 
 CreateConVar( "etb_vignette", 1, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY}, "0 = OFF // 1 = On // At certain value of health will the screen become darker?" )
 
-CreateConVar( "etb_threshold", 35, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY}, "At this point, sounds and screen will getting stronger" )
+CreateConVar( "etb_threshold", 65, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY}, "At this point, sounds and screen will getting stronger" )
 
 CreateConVar( "etb_muffle_sound", 0, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY}, "0 = OFF // 1 = On // At certain value of health will the sound become muffled?" )
 
-CreateConVar( "etb_muffle_effect", 35, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY}, "Health value that the sounds will become muffled" )
+CreateConVar( "etb_muffle_effect", 95, {FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY}, "Health value that the sounds will become muffled" )
 
 local etb_healtheffect_system = GetConVar( "etb_healtheffect_system" )
 local etb_heartbeat_sound = GetConVar( "etb_heartbeat_sound" )
@@ -59,6 +59,7 @@ if CLIENT then
 	}
 
 	local function LowHP_HUDPaint()
+
 		if etb_healtheffect_system:GetInt() == 0 then
 			return 
 		end
@@ -69,7 +70,7 @@ if CLIENT then
 		local FT = FrameTime()
 		
 		if etb_muffle_sound:GetInt() then
-			if ply:Health() <= etb_muffle_effect:GetInt() then
+			if ply:Health() + 30 <= etb_muffle_effect:GetInt() then
 				if not ply.lastDSP then
 					ply:SetDSP(14)
 					ply.lastDSP = 14
@@ -99,13 +100,13 @@ if CLIENT then
 				
 				if etb_heartbeat_sound:GetInt() == 1 then
 					if CT > hpwait then
-						ply:EmitSound("lowhp/hbeat.wav", 70 * intensity, 100 + 20 * intensity)
-						hpwait = CT + 0.5
+						ply:EmitSound("lowhp/hbeat.wav", 100 * intensity + 20, 100 + 80 * intensity)
+						hpwait = CT + 0.8
 					end
 				end
 				
 				if etb_redflash:GetInt() == 1 then
-					surface.SetDrawColor(255, 0, 0, (50 * intensity) * hpalpha)
+					surface.SetDrawColor(200, 0, 0, math.max(80 * intensity * hpalpha, 0.5))
 					surface.DrawTexturedRect(0, 0, x, y)
 					
 					if CT < hpwait - 0.4 then
